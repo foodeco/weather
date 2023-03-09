faf8c4a5d03e97876838027ec5eaac5b59580712
서동욱 <seodong-ug@seodong-ug-ui-MacBookAir.local>
2023-03-09 15:59:15 +0900

    check

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import CurrentLocation from './components/CurrentLocation';
 import Headers from './components/Headers';
+import Map from './components/Map';
 import SearchLocation from './components/SearchLocation';
 
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<CurrentLocation />} />
           <Route path="/search/:city" element={<SearchLocation />} />
         </Routes>
+      <Map />
       </BrowserRouter>
     </div>
   );
